721dab76bed0e2f519ed896e8c4f3a402316a349
Zhihui Tang <crafttang@gmail.com>
2016-11-10 09:15:07 +0100

    add sort demo

---
+++ app/src/main/java/com/max/tang/demokiller/activity/SortActivity.java
@@ -0,0 +1,14 @@
+package com.max.tang.demokiller.activity;
+
+import android.os.Bundle;
+
+import com.max.tang.demokiller.R;
+
+public class SortActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sort);
+    }
+}
--- app/src/main/java/com/max/tang/demokiller/utils/DataLoader.java
+++ app/src/main/java/com/max/tang/demokiller/utils/DataLoader.java
@@ -3,6 +3,7 @@ package com.max.tang.demokiller.utils;
 import com.max.tang.demokiller.activity.DemoDataBindingActivity;
 import com.max.tang.demokiller.activity.LoginActivity;
 import com.max.tang.demokiller.activity.RxActivity;
+import com.max.tang.demokiller.activity.SortActivity;
 import com.max.tang.demokiller.activity.WatchActivity;
 import com.max.tang.demokiller.model.DemoEntity;
 
@@ -25,6 +26,7 @@ public class DataLoader {
     //获取本地测试的Activity列表
     public List<DemoEntity> getTestList() {
         List<DemoEntity> list = new ArrayList<>();
+        list.add(new DemoEntity("Sort Demo", SortActivity.class));
         list.add(new DemoEntity("Login Demo", LoginActivity.class));
         list.add(new DemoEntity("Watch Activity", WatchActivity.class));
         list.add(new DemoEntity("Data Binding", DemoDataBindingActivity.class));
