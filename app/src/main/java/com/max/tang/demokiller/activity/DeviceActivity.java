8f7237f6c72acc66e08414b2ad936ca0ab481575
Zhihui Tang <crafttang@gmail.com>
2016-11-28 23:04:43 +0100

    add demo

--- app/src/main/java/com/max/tang/demokiller/activity/DeviceActivity.java
+++ app/src/main/java/com/max/tang/demokiller/activity/DeviceActivity.java
@@ -1,12 +1,15 @@
 package com.max.tang.demokiller.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import com.max.tang.demokiller.R;
 import com.max.tang.demokiller.utils.device.AndroidSettingsUtil;
 import com.max.tang.demokiller.utils.log.Logger;
+import java.util.Locale;
 
 public class DeviceActivity extends AppCompatActivity {
     final String TAG = "device";
@@ -14,11 +17,21 @@ public class DeviceActivity extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device);
+        permissionToDrawOverlays();
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Logger.d(TAG, "onActivityResult: " + requestCode );
+
+        if (requestCode == PERM_REQUEST_CODE_DRAW_OVERLAYS) {
+            if (android.os.Build.VERSION.SDK_INT >= 23) {   //Android M Or Over
+                if (!Settings.canDrawOverlays(this)) {
+                    // ADD UI FOR USER TO KNOW THAT UI for SYSTEM_ALERT_WINDOW permission was not granted earlier...
+                    Logger.d(TAG, "this shit UI: " );
+                }
+            }
+        }
     }
 
     public void openLocationSetting(View view) {
@@ -29,4 +42,30 @@ public class DeviceActivity extends AppCompatActivity {
     public void openPrivacySettings(View view) {
         AndroidSettingsUtil.openPrivacySettings(this, 91);
     }
+
+    public void getLanguageCode(View view) {
+        String lan = Locale.getDefault().getDisplayLanguage();
+        String lanCode = Locale.getDefault().getLanguage();
+        Logger.d(TAG, "getLanguage: " + lanCode);
+        Logger.d(TAG, "getDisplayLanguage: " + lan);
+    }
+
+    public final static int PERM_REQUEST_CODE_DRAW_OVERLAYS = 1234;
+    /**
+     * Permission to draw Overlays/On Other Apps, related to 'android.permission.SYSTEM_ALERT_WINDOW' in Manifest
+     * Resolves issue of popup in Android M and above "Screen overlay detected- To change this permission setting you first have to turn off the screen overlay from Settings > Apps"
+     * If app has not been granted permission to draw on the screen, create an Intent &
+     * set its destination to Settings.ACTION_MANAGE_OVERLAY_PERMISSION &
+     * add a URI in the form of "package:<package name>" to send users directly to your app's page.
+     * Note: Alternative Ignore URI to send user to the full list of apps.
+     */
+    public void permissionToDrawOverlays() {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {   //Android M Or Over
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, PERM_REQUEST_CODE_DRAW_OVERLAYS);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/max/tang/demokiller/view/HistogramView.java
+++ app/src/main/java/com/max/tang/demokiller/view/HistogramView.java
@@ -11,6 +11,7 @@ import com.max.tang.demokiller.R;
 import com.max.tang.demokiller.utils.SizeUtil;
 import com.max.tang.demokiller.utils.log.Logger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -18,112 +19,129 @@ import java.util.List;
  */
 
 public class HistogramView extends View {
-  private Paint mPaint;
-  private String TAG = "shanghai";
-  private float mPadding; //边距
-  private int mColorBar;
+    private Paint mPaint;
+    private String TAG = "shanghai";
+    private float mPadding; //边距
+    private int mBarColor;
+    private int mBarWidth = 20;
 
-  private int mWidth;
-  private int mHeight;
-  private List<Integer> mData;
+    private int mWidth;
+    private int mHeight;
+    private List<Integer> mData;
 
-  public HistogramView(Context context) {
-    super(context);
-  }
-
-  public HistogramView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    obtainStyledAttrs(attrs);
-    init();
-  }
-
-  private void obtainStyledAttrs(AttributeSet attrs) {
-    TypedArray array = null;
-    try {
-      array = getContext().obtainStyledAttributes(attrs, R.styleable.Histogram);
-      mPadding =
-          array.getDimension(R.styleable.Histogram_padding, SizeUtil.Dp2Px(getContext(), 10));
-      mColorBar = array.getColor(R.styleable.Histogram_color_bar, Color.argb(225, 0, 0, 0));
-    } catch (Exception e) {
-      //一旦出现错误全部使用默认值
-      mPadding = SizeUtil.Dp2Px(getContext(), 10);
-      mColorBar = Color.RED;
-    } finally {
-      if (array != null) {
-        array.recycle();
-      }
-    }
-  }
-
-  public HistogramView(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-  }
-
-  @Override protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-    drawCoordinates(canvas);
-  }
-
-  private void init() {
-    mPaint = new Paint();
-    mPaint.setAntiAlias(true);
-    mPaint.setDither(true);
-
-    mData = new ArrayList<>();
-    for (int i = 0; i < 20; i++) {
-      mData.add((int)(Math.random()*100));
+    public HistogramView(Context context) {
+        super(context);
     }
 
-  }
-
-  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    int width = 2000; //设定一个最小值
-
-    int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-    int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-    int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-    int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-
-    if (widthMode == MeasureSpec.AT_MOST
-        || widthMode == MeasureSpec.UNSPECIFIED
-        || heightMeasureSpec == MeasureSpec.AT_MOST
-        || heightMeasureSpec == MeasureSpec.UNSPECIFIED) {
-      Logger.e(TAG, "onMeasure: 宽度高度至少有一个确定的值,不能同时为wrap_content");
-    } else { //至少有一个为确定值,要获取其中的最小值
-      if (widthMode == MeasureSpec.EXACTLY) {
-        width = Math.min(widthSize, width);
-      }
-      if (heightMode == MeasureSpec.EXACTLY) {
-        width = Math.min(heightSize, width);
-      }
+    public HistogramView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        obtainStyledAttrs(attrs);
+        init();
     }
-    Logger.i(TAG, "onMeasure: " + width);
-    mWidth = width;
-    mHeight = width;
-    setMeasuredDimension(width, width);
-  }
 
-  private void drawCoordinates(Canvas canvas) {
+    private void obtainStyledAttrs(AttributeSet attrs) {
+        TypedArray array = null;
+        try {
+            array = getContext().obtainStyledAttributes(attrs, R.styleable.Histogram);
+            mPadding =
+                array.getDimension(R.styleable.Histogram_padding, SizeUtil.Dp2Px(getContext(), 10));
+            mBarColor = array.getColor(R.styleable.Histogram_color_bar, Color.argb(225, 0, 0, 0));
+        } catch (Exception e) {
+            //一旦出现错误全部使用默认值
+            mPadding = SizeUtil.Dp2Px(getContext(), 10);
+            mBarColor = Color.RED;
+        } finally {
+            if (array != null) {
+                array.recycle();
+            }
+        }
+    }
 
-    // X轴
-    Logger.i(TAG, "drawCoordinates");
-    canvas.drawLine(getPaddingLeft(), mHeight - getPaddingBottom(), mWidth - getPaddingRight(),
-        mHeight - getPaddingBottom(), mPaint);
-    // X轴上的箭头
-    canvas.drawLine(mWidth - getPaddingRight() - 20, mHeight - getPaddingBottom() - 10,
-        mWidth - getPaddingRight(), mHeight - getPaddingBottom(), mPaint);
-    canvas.drawLine(mWidth - getPaddingRight() - 20, mHeight - getPaddingBottom() + 10,
-        mWidth - getPaddingRight(), mHeight - getPaddingBottom(), mPaint);
+    public HistogramView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 
-    // 绘制Y轴
-    canvas.drawLine(getPaddingLeft(), getPaddingTop(), getPaddingLeft(),
-        mHeight - getPaddingBottom(), mPaint);
+    @Override protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawCoordinates(canvas);
+        int x = getPaddingLeft();
+        int y = mHeight - getPaddingBottom();
+        int itemPadding = 5;
+        int maxItem = Collections.max(mData);
+        float scaleY = (mHeight-100)/maxItem;
+        float scaleX = mWidth/( mData.size()+2);
 
-    // Y轴上的箭头
-    canvas.drawLine(getPaddingLeft() - 10, getPaddingTop() + 20, getPaddingLeft(), getPaddingTop(),
-        mPaint);
-    canvas.drawLine(getPaddingLeft() + 10, getPaddingTop() + 20, getPaddingLeft(), getPaddingTop(),
-        mPaint);
-  }
+        mPaint.setStrokeWidth(mBarWidth);
+        //mPaint.setColor(ContextCompat.getColor(getContext(), R.color.cpb_blue));
+        mPaint.setColor(mBarColor);
+        for (int i = 0; i < mData.size(); i++) {
+            canvas.drawLine(x + (i+1)*scaleX, y, x + (i+1)*scaleX,
+                y - mData.get(i)*scaleY, mPaint);
+        }
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+
+        mData = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mData.add((int) (Math.random() * 100));
+        }
+    }
+
+    @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = 2000; //设定一个最小值
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        if (widthMode == MeasureSpec.AT_MOST
+            || widthMode == MeasureSpec.UNSPECIFIED
+            || heightMeasureSpec == MeasureSpec.AT_MOST
+            || heightMeasureSpec == MeasureSpec.UNSPECIFIED) {
+            Logger.e(TAG, "onMeasure: 宽度高度至少有一个确定的值,不能同时为wrap_content");
+        } else { //至少有一个为确定值,要获取其中的最小值
+            if (widthMode == MeasureSpec.EXACTLY) {
+                width = Math.min(widthSize, width);
+            }
+            if (heightMode == MeasureSpec.EXACTLY) {
+                width = Math.min(heightSize, width);
+            }
+        }
+        Logger.i(TAG, "onMeasure: " + width);
+        mWidth = width;
+        mHeight = width;
+        setMeasuredDimension(width, width);
+    }
+
+    private void drawCoordinates(Canvas canvas) {
+
+        // X轴
+        mPaint.setStrokeWidth(2);
+        Logger.i(TAG, "drawCoordinates");
+        canvas.drawLine(getPaddingLeft(), mHeight - getPaddingBottom(), mWidth - getPaddingRight(),
+            mHeight - getPaddingBottom(), mPaint);
+        // X轴上的箭头
+        /*
+        canvas.drawLine(mWidth - getPaddingRight() - 20, mHeight - getPaddingBottom() - 10,
+            mWidth - getPaddingRight(), mHeight - getPaddingBottom(), mPaint);
+        canvas.drawLine(mWidth - getPaddingRight() - 20, mHeight - getPaddingBottom() + 10,
+            mWidth - getPaddingRight(), mHeight - getPaddingBottom(), mPaint);
+
+        // 绘制Y轴
+        canvas.drawLine(getPaddingLeft(), getPaddingTop(), getPaddingLeft(),
+            mHeight - getPaddingBottom(), mPaint);
+
+        // Y轴上的箭头
+        canvas.drawLine(getPaddingLeft() - 10, getPaddingTop() + 20, getPaddingLeft(),
+            getPaddingTop(), mPaint);
+        canvas.drawLine(getPaddingLeft() + 10, getPaddingTop() + 20, getPaddingLeft(),
+            getPaddingTop(), mPaint);
+            */
+    }
 }
